7d41679d80ac520c8d376049f4f7c513804d8ac1
Alejandro Rodriguez Moreno <alejandro.rodriguezm.external@atos.net>
2021-12-01 12:26:46 +0100

    primer stater implement

---
+++ src/main/java/com/example/ImpStarter/ImpStarterApplication.java
@@ -0,0 +1,13 @@
+package com.example.ImpStarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImpStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImpStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ImpStarter/ImpStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ImpStarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImpStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
